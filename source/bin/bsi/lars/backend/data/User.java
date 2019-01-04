b557bece904b95f94fd5f72e559c4bdde7c24aed
Mahlodi25 <46344331+Mahlodi25@users.noreply.github.com>
2019-01-04 07:50:46 +0200

    Add files via upload

---
+++ source/bin/bsi/lars/backend/data/User.java
@@ -0,0 +1,81 @@
+package bsi.lars.backend.data;
+
+/**
+ * Representation of a user. Represents the data of the database.
+ * 
+ *
+ */
+public class User {
+	private int id;
+	private String longname;
+	private String shortname;
+	private boolean is_editor;
+	private boolean is_admin;
+	private boolean is_viewer;
+	private String userpw_hash;
+	private String userlogin_name;
+
+	public User(int id, String longname, String shortname, boolean is_editor, boolean is_admin, boolean is_viewer, String userpw_hash, String userlogin_name) {
+		this.id             = id            ;
+		this.longname       = longname      ;
+		this.shortname      = shortname     ;
+		this.is_editor      = is_editor     ;
+		this.is_admin       = is_admin      ;
+		this.is_viewer      = is_viewer     ;
+		this.userpw_hash    = userpw_hash   ;
+		this.userlogin_name = userlogin_name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getLongname() {
+		return longname;
+	}
+
+	public String getShortname() {
+		return shortname;
+	}
+
+	public boolean isEditor() {
+		return is_editor;
+	}
+
+	public boolean isAdmin() {
+		return is_admin;
+	}
+
+	public boolean isViewer() {
+		return is_viewer;
+	}
+
+	public String getUserpw_hash() {
+		return userpw_hash;
+	}
+
+	public String getUserlogin_name() {
+		return userlogin_name;
+	}
+
+	@Override
+	public String toString() {
+		return longname;
+	}
+
+	public String toXML() {
+		return "<user>" + toCData(getLongname()) + "</user>";
+	}
+
+	protected String toCData(String data) {
+		return "<![CDATA["+data+"]]>";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof User) {
+			return ((User) obj).getId() == getId();
+		}
+		return false;
+	}
+}
